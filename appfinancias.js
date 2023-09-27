a30901d47c0f03acbc1da9cd0b7b7788f715ae0b
Lucas Arantes <aranteslucas07@gmail.com>
2023-09-26 23:33:33 -0300

    first commit

---
+++ appfinancias.js
@@ -0,0 +1,19 @@
+let receitasConta = 30000
+let despesasConta = 6000
+let saldoEmConta = receitasConta - despesasConta
+let metaCasaNova = 3000000
+let dinheiroGuardado = 1500000
+let faltaBater = 1500000
+let patrimonio = 2000000
+let investimentos = 500000
+let dividendosMensais = 5000
+let metaDividendosMensais = 50000
+
+/* Os códigos abaixo são apenas um esboço para que eu possa melhorar mais e mais. 
+Dependendo do que for, irei melhorar mais e mai. */
+console.log(receitasConta)
+console.log(despesasConta)
+console.log(saldoEmConta = receitasConta - despesasConta)
+
+console.log('Os recursos necessários para construir sua casa são de:',
+metaCasaNova , 'milhões.', 'Você tem:', dinheiroGuardado, 'Ainda falta:', metaCasaNova - dinheiroGuardado)
